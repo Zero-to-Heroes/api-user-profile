4b7c082c51c6563ea8a507f33233e7dd25f81eb0
sebastientromp <sebastien.tromp@gmail.com>
2023-04-25 10:28:14 +0200

    update webpack deps to 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const { readFileSync } = require('fs');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
@@ -12,7 +13,9 @@ const entries = Object.values(cfn.Resources)
 	// Find nodejs functions
 	.filter(v => v.Type === 'AWS::Serverless::Function')
 	.filter(
-		v => (v.Properties.Runtime && v.Properties.Runtime.startsWith('nodejs')) || (!v.Properties.Runtime && cfn.Globals.Function.Runtime),
+		v =>
+			(v.Properties.Runtime && v.Properties.Runtime.startsWith('nodejs')) ||
+			(!v.Properties.Runtime && cfn.Globals.Function.Runtime),
 	)
 	.map(v => ({
 		// Isolate handler src filename
@@ -32,12 +35,11 @@ const entries = Object.values(cfn.Resources)
 		{},
 	);
 
-
 module.exports = {
 	// http://codys.club/blog/2015/07/04/webpack-create-multiple-bundles-with-entry-points/#sec-3
 	entry: entries,
 	target: 'node',
-	mode: conf.prodMode ? 'production' : 'development',
+	mode: 'production',
 	module: {
 		rules: [
 			{
